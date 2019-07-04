5df524d6f356357c5065ab707f43c81390bc641a
Ryan Yang <javathon@gmail.com>
2019-07-03 18:10:04 -0700

    using defualt material ui cards for units

--- src/components/Unit.js
+++ src/components/Unit.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Units as UnitData} from '../assets/UnitSets';
-import {Card, CardHeader, Avatar, CardMedia, CardContent, Typography} from '@material-ui/core';
+import {Card, CardHeader, CardMedia, CardContent, Typography} from '@material-ui/core';
 import './Units.css';
 
 export class Unit extends Component {
@@ -17,22 +17,18 @@ export class Unit extends Component {
             <div className="unit-holder">
                 <Card>
                 <CardHeader
-                    avatar={
-                    <Avatar aria-label="Recipe" >
-                        
-                    </Avatar>
-                    }
+                    avatar={<img src={s.icon} className="unit-avatar"></img>}
                     title={s.name}
                     subheader={s.cost + " Gold"}
                 />
                 <CardMedia
+                    className="unit-splash"
                     image={s.splash}
-                    title="Paella dish"
+                    title={s.name + " splash art"}
                 />
                 <CardContent>
                     <Typography variant="body2" color="textSecondary" component="p">
-                    This impressive paella is a perfect party dish and a fun meal to cook together with your
-                    guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                    {s.ability}
                     </Typography>
                 </CardContent>
                 </Card>
