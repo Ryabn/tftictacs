061420118002ba60b0d454b280e40a5f6dc457c8
Ryan Yang <javathon@gmail.com>
2019-07-28 05:16:58 -0700

    Updates

---
+++ precache-manifest.81286e82e4ab19bc82be54c805cabd2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc99de57bd038c78cb58dcfae62aba6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5debcd56ece165e4301c",
+    "url": "/static/css/main.9af2a47c.chunk.css"
+  },
+  {
+    "revision": "0bfc3b1fe2893496111e",
+    "url": "/static/js/2.ee48c077.chunk.js"
+  },
+  {
+    "revision": "5debcd56ece165e4301c",
+    "url": "/static/js/main.17420e27.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8172b701a066c73b848eb83803341301.js"
+  "/precache-manifest.81286e82e4ab19bc82be54c805cabd2a.js"
 );
 
 self.addEventListener('message', (event) => {
